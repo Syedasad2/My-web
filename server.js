a0c1f1791ec27739c6d8eaa9fe9c85317a7b7594
Syedasad2 <154581585+Syedasad2@users.noreply.github.com>
2025-04-28 15:03:25 +0500

    changes

--- server.js
+++ server.js
@@ -10,43 +10,40 @@ const session = require("express-session");
 // Routers
 const adminRouter = require("./routes/adminRouter");
 const userRouter = require("./routes/userRouter");
+// const guarantorRouter = require("./routes/GuarantorRouter");
 const loanRouter = require("./routes/loanRouter");
 
 const app = express();
+
 const PORT = 4003;
 
-// ✅ CORS config
-app.use(cors({
-  origin: ["http://localhost:5173", "https://new-web1.netlify.app/"], // jitne chahie wo yahan dal lo
-  credentials: true,  // Cookies allow karne ke liye
-}));
-
 // Middleware
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
+app.use(cookieParser()); // Correct cookie parsing
 app.use(flash());
 
 // Session Configuration
 app.use(
   session({
-    secret: process.env.SECRET_KEY,
+    secret: process.env.SECRET_KEY, 
     resave: false,
     saveUninitialized: true,
     cookie: {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production" ? true : false,
-    },
+    }, 
   })
 );
 
-// Static files
 app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "ejs");
 
 // Routes
 app.use("/admin", adminRouter);
 app.use("/users", userRouter);
+// app.use("/guarantor", guarantorRouter);
 app.use("/loan", loanRouter);
 
 // Default Route
