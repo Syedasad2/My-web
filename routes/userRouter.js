50be11928991faca7037e7b5039d352b0f370098
Syedasad2 <154581585+Syedasad2@users.noreply.github.com>
2025-04-30 16:19:22 +0500

    some changes

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const { registerUser, loginUser, logoutUser, getAllUsers, getUserDashboard } = require("../controllers/userController");
-const isloggedIn = require("../middlewares/isloggedIn");
+// const isloggedIn = require("../middlewares/isloggedIn");
 
 const router = express.Router();
 
@@ -14,9 +14,9 @@ router.post("/login", loginUser);
 router.post("/logout", logoutUser);
 
 // Get All Users (for Admin or Admin-like roles)
-router.get("/", isloggedIn, getAllUsers);  // Protected route for admins or authenticated users
+router.get("/", getAllUsers);  // Protected route for admins or authenticated users
 
 // User Dashboard (protected route)
-router.get("/dashboard", isloggedIn, getUserDashboard);  // Only accessible if logged in
+router.get("/dashboard", getUserDashboard);  // Only accessible if logged in
 
 module.exports = router;
